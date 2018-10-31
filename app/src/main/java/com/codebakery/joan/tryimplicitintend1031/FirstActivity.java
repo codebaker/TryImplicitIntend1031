97a50fd5b1791847cb1464b46fb3e3181a5505fd
codebaker <jungah.kim76@gmail.com>
2018-10-31 10:51:33 +0900

    Initial commit

---
+++ app/src/main/java/com/codebakery/joan/tryimplicitintend1031/FirstActivity.java
@@ -0,0 +1,28 @@
+package com.codebakery.joan.tryimplicitintend1031;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class FirstActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText editTextValue = findViewById(R.id.editTextValue);
+
+        Bundle bundle = new Bundle();
+        bundle.putString("result", editTextValue.getText().toString());
+        Intent intent = new Intent(this,SecondActivity.class)
+                .putExtras(bundle);
+        startActivity(intent);
+    }
+
+}
